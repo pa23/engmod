c0b9f86788e7a7e0600644713cad5ed5c113d0c1
Artem Petrov <pa2311@gmail.com>
2012-03-04 12:03:48 +0400

    Code style fixed.

--- strop.h
+++ strop.h
@@ -1,21 +1,21 @@
 /*
-    engmod
-    Calculation of engine modes distribution field.
+  engmod
+  Calculation of engine modes distribution field.
 
-    File: strop.h
+  File: strop.h
 
-    Copyright (C) 2012 Artem Petrov <pa2311@gmail.com>
+  Copyright (C) 2012 Artem Petrov <pa2311@gmail.com>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef STROP_H
